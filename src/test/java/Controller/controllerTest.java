a319f94c55bcfe6eedc1a132a730d87ddb4d1010
choijungwoo493 <jwoo1016@gmail.com>
2022-07-05 17:45:57 +0900

    SONAR_HOST,SONAR_TOKEN 추가

--- src/test/java/Controller/controllerTest.java
+++ src/test/java/Controller/controllerTest.java
@@ -1,8 +1,27 @@
 package Controller;
 
 
+import org.junit.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 
 class controllerTest {
+
+    @Test
+    void skdsk(){
+        assertThat(1).isEqualTo(1);
+
+    }
+
+    @Test
+    void sd(){
+        assertThat(1).isEqualTo(1);
+
+    }
+    @Test
+    void sds(){
+        assertThat(1).isEqualTo(1);
+
+    }
 
 }
